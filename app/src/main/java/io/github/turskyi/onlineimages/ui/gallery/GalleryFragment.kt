a36480724d39347f5ca3084abb119f7de0a77ae2
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2021-06-12 14:19:49 +0300

    update dependencies

--- app/src/main/java/io/github/turskyi/onlineimages/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/io/github/turskyi/onlineimages/ui/gallery/GalleryFragment.kt
@@ -3,11 +3,13 @@ package io.github.turskyi.onlineimages.ui.gallery
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,57 +19,27 @@ import io.github.turskyi.onlineimages.databinding.FragmentGalleryBinding
 
 // We need to pass the layout into the constructor in order for to navigation will work
 @AndroidEntryPoint
-class GalleryFragment : Fragment(R.layout.fragment_gallery),OnItemClickListener {
+class GalleryFragment : Fragment(R.layout.fragment_gallery), OnItemClickListener {
     private val viewModel by viewModels<GalleryViewModel>()
 
     private var _binding: FragmentGalleryBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: PhotoAdapter
+    private lateinit var searchView: SearchView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentGalleryBinding.bind(view)
-        val adapter = PhotoAdapter(this)
-        binding.apply {
-            recyclerView.setHasFixedSize(true)
-            // getting rid of redundant flashes during changing the search query
-            recyclerView.itemAnimator = null
-            recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
-                header = PhotoLoadStateAdapter { adapter.retry() },
-                footer = PhotoLoadStateAdapter { adapter.retry() },
-            )
-            buttonRetry.setOnClickListener {
-                adapter.retry()
-            }
-        }
+        initView(view)
+
+        // init observers
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
-
-
-        adapter.addLoadStateListener { loadState ->
-            binding.apply {
-                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
-                recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
-                buttonRetry.isVisible = loadState.source.refresh is LoadState.Error
-                textViewError.isVisible = loadState.source.refresh is LoadState.Error
-
-                // empty view
-                if (loadState.source.refresh is LoadState.NotLoading &&
-                    loadState.append.endOfPaginationReached &&
-                    adapter.itemCount < 1) {
-                    recyclerView.isVisible = false
-                    textViewEmpty.isVisible = true
-                } else {
-                    textViewEmpty.isVisible = false
-                }
-            }
-        }
-        setHasOptionsMenu(true)
     }
 
-
     override fun onItemClick(photo: PhotoResponse) {
-        val action = GalleryFragmentDirections.actionGalleryFragmentToDetailsFragment(photo)
+        val action: NavDirections = GalleryFragmentDirections
+            .actionGalleryFragmentToDetailsFragment(photo)
         findNavController().navigate(action)
     }
 
@@ -76,8 +48,54 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery),OnItemClickListener 
 
         inflater.inflate(R.menu.menu_gallery, menu)
 
-        val searchItem = menu.findItem(R.id.action_search)
-        val searchView = searchItem.actionView as SearchView
+        val searchItem: MenuItem = menu.findItem(R.id.action_search)
+        if (searchItem.actionView is SearchView) {
+            searchView = searchItem.actionView as SearchView
+            initListeners(adapter)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initView(view: View) {
+        _binding = FragmentGalleryBinding.bind(view)
+        adapter = PhotoAdapter(this)
+        binding.apply {
+            recyclerView.setHasFixedSize(true)
+            // getting rid of redundant flashes during changing the search query
+            recyclerView.itemAnimator = null
+            recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = PhotoLoadStateAdapter { adapter.retry() },
+                footer = PhotoLoadStateAdapter { adapter.retry() },
+            )
+        }
+        setHasOptionsMenu(true)
+    }
+
+    private fun initListeners(adapter: PhotoAdapter) {
+        binding.buttonRetry.setOnClickListener { adapter.retry() }
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+                buttonRetry.isVisible = loadState.source.refresh is LoadState.Error
+                textViewError.isVisible = loadState.source.refresh is LoadState.Error
+
+                // controlling when empty view should be visible
+                if (loadState.source.refresh is LoadState.NotLoading &&
+                    loadState.append.endOfPaginationReached &&
+                    adapter.itemCount < 1
+                ) {
+                    recyclerView.isVisible = false
+                    textViewEmpty.isVisible = true
+                } else {
+                    textViewEmpty.isVisible = false
+                }
+            }
+        }
 
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -94,10 +112,5 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery),OnItemClickListener 
                 return true
             }
         })
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
