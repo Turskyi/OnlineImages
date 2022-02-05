507658452a5fd2a63a04d48a302889488601ef0d
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2022-02-05 18:48:11 -0500

    update February 2021

--- app/src/main/java/io/github/turskyi/onlineimages/data/entities/UnsplashResponse.kt
+++ app/src/main/java/io/github/turskyi/onlineimages/data/entities/UnsplashResponse.kt
@@ -1,7 +1,3 @@
 package io.github.turskyi.onlineimages.data.entities
 
-import io.github.turskyi.onlineimages.data.entities.PhotoResponse
-
-data class UnsplashResponse(
-    val results: List<PhotoResponse>
-)
+data class UnsplashResponse(val results: List<PhotoResponse>)
